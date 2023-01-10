d8208ca9c24280b12b28a432bcff27457f54c1b2
seyoungOh <osy9536@naver.com>
2023-01-10 20:37:55 +0900

    [백준] 2744 대소문자 바꾸기

---
+++ Algorithm/src/LowerUpperChange.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LowerUpperChange {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        StringBuilder sb = new StringBuilder();
+        for(int i= 0;i<s1.length();i++){
+            if(s1.charAt(i)>='a' && s1.charAt(i)<='z'){
+                sb.append(s1.toUpperCase().charAt(i));
+            }
+            else if(s1.charAt(i)>='A' && s1.charAt(i)<='Z'){
+                sb.append(s1.toLowerCase().charAt(i));
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
