ce3726923d0730e4adf40aceeb54bc9518a10ef6
seyoungOh <osy9536@naver.com>
2023-01-11 20:51:38 +0900

    no message

--- String/LowerUpperChange.java
+++ String/LowerUpperChange.java
@@ -1,3 +1,5 @@
+package String;
+
 import java.util.*;
 
 public class LowerUpperChange {
--- String/NumWord.java
+++ String/NumWord.java
@@ -1,3 +1,5 @@
+package String;
+
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
