dabab31e2dec79b20789626bc8fe46b2948b0499
seyoungOh <osy9536@naver.com>
2023-01-11 20:15:26 +0900

    [백준] 1157 단어 공부

---
+++ Algorithm/src/manyWord.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class manyWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[26];
+
+        int max = -1;
+        char result = '?';
+
+        String s = sc.next();
+        s = s.toUpperCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'A']++;
+            if (max < num[s.charAt(i) - 'A']) {
+                max = num[s.charAt(i) - 'A'];
+                result = s.charAt(i);
+            } else if (max == num[s.charAt(i) - 'A']) {
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
+    }
+}
