ac248536366ad61718fe5dd1d17069e23edc5c25
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-20 00:23:13 +0900

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        for(int i = 0; i<babbling.length; i++){
-            String[] splitBabbling = babbling[i].split("aya|ye|woo|ma");
-            if(splitBabbling.length==0){
+        for(int i=0; i<babbling.length; i++){
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i]=babbling[i].trim();
+            if (babbling[i].isEmpty()) {
                 answer++;
             }
         }
