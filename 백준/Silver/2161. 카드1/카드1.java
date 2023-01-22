45b61d4c8f5257253a573dfe930802965f645a7b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-22 22:51:41 +0900

    [Silver V] Title: 카드1, Time: 136 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -1,12 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int j =0;
         StringBuilder sb= new StringBuilder();
         List<Integer> list = new ArrayList<>();
