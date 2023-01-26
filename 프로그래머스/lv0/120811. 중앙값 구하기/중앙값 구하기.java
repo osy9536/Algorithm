98c34da9031be0d33d1b03bb69d1494abdce0201
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-26 15:26:11 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.28 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,8 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        Arrays.sort(array);
-        answer=array[array.length/2];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        Collections.sort(list);
+        answer = list.get(list.size() / 2);
         return answer;
     }
 }
