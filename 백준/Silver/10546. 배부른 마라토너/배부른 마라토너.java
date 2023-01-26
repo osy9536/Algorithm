26ebaf33963ad4bd202e46fcce3e1b5c6b3a82fc
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-26 20:03:27 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 416 ms, Memory: 34548 KB -BaekjoonHub

--- 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Map<String, Integer> map = new HashMap<>(n);
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        int inputSize = 2*N-1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < inputSize; i++) {
             String s = br.readLine();
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
-        for (int i = 1; i < n; i++) {
-            String s = br.readLine();
-            if (map.get(s) == 1) {
+            if(map.containsKey(s)) {
                 map.remove(s);
-            } else {
-                map.put(s, map.get(s)-1);
+            }
+            else {
+                map.put(s, 1);
             }
         }
-        for (String s : map.keySet()) {
-            System.out.print(s);
-        }
+        String s = map.toString();
+        s = s.split("=")[0];
+        System.out.println(s.substring(1, s.length()));
     }
 }
