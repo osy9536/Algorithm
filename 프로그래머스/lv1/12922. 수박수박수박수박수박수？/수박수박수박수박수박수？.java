f0b0d1f047040daf6b1e1268836bb344c2f1197b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 09:59:46 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.15 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,16 +1,16 @@
 class Solution {
-    public String  solution(int n){
-        String answer = "";
+    public static String  solution(int n){
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if(answer.equals("")){
-                answer += "수";
+            if(answer.toString().equals("")){
+                answer.append("수");
             }
             else if(answer.charAt(i-1)=='수'){
-                answer += "박";
+                answer.append("박");
             } else if (answer.charAt(i-1)=='박') {
-                answer += "수";
+                answer.append("수");
             }
         }
-        return answer;
+        return answer.toString();
     }
 }
