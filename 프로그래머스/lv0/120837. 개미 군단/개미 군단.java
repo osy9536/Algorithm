34ebc99d454f4b4183d0edfd503e51262a890a49
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 19:17:19 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,13 +1,11 @@
 class Solution {
     public static int solution(int hp) {
         int answer = 0;
-        int damage= 5;
-        while (hp>0) {
-            if(3<=hp&&hp<5) damage=3;
-            else if(hp<3) damage=1;
-            answer += hp / damage;
-            hp=hp%damage;
-        }
+        answer=hp/5;
+        hp=hp%5;
+        answer+=hp/3;
+        hp=hp%3;
+        answer+= hp;
         return answer;
     }
 }
