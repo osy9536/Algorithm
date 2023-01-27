4d09f7e6e1b4f94bc193ab24c0679c7da257b39b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 18:00:35 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
