97f816a461bdeb95ab9ce3918248295930a46d4f
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 10:59:43 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -2,16 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 class Solution {
-    public int[] solution(long n ){
+    public List<Integer> solution(long n ){
         List<Integer> list = new ArrayList<>();
         while (n > 0) {
             list.add((int) (n % 10));
             n/=10;
         }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        return list;
     }
 }
