2925eb66de8485a25ded0377a03207ae0f9a7b1d
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 15:03:28 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i : d) {
+            if (budget < i) {
+                break;
+            }
+            answer++;
+            budget -= i;
+        }
+        return answer;
+    }
+}
