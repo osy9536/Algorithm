ca07c8f9a01a3c52c4618ddb709f12da4aa4ceb9
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-27 10:23:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.39 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i <2*participant.length-1; i++) {
+            if(i<participant.length){
+                if (map.containsKey(participant[i])) {
+                    map.remove(participant[i]);
+                } else map.put(participant[i], 1);
+            }
+            else {
+                if (map.containsKey(completion[i-participant.length])) {
+                    map.remove(completion[i-participant.length]);
+                } else map.put(completion[i-participant.length], 1);
+            }
+
+        }
+        answer=map.toString();
+        answer=answer.split("=")[0];
+        return answer.substring(1,answer.length());
+    }
+}
