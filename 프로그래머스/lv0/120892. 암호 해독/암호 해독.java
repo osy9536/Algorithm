232a1204799e42992a2fa5c89831cbbaa2793e5c
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-28 19:40:21 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,9 +1,6 @@
 class Solution {
     public static StringBuilder solution(String cipher, int code) {
         StringBuilder answer = new StringBuilder();
-        if (cipher.length() >= code) {
-
-        }
         for (int i = code-1; i < cipher.length(); i=i+code) {
                 answer.append(cipher.charAt(i));
         }
