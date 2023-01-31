c92dfcd4d1b2d177f84d622dd987dac06f3ad990
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-31 09:43:21 +0900

    [level 1] Title: 폰켓몬, Time: 5.47 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,14 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 class Solution {
     public static int solution(int[] nums) {
-        int answer = 0;
         Map<Integer, Boolean> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], true);
         }
         if (map.size() > nums.length / 2) {
-            answer = nums.length / 2;
-        } else answer = map.size();
-        return answer;
+            return nums.length / 2;
+        } else return map.size();
     }
 }
