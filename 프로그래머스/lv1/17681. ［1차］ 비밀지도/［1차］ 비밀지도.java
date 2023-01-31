0767b608766c5f4dd3d33f6f442a0c3501f99c92
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-01-31 10:30:11 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.93 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public static StringBuilder[] solution(int n, int[] arr1, int[] arr2) throws NullPointerException {
+        StringBuilder[] answer = new StringBuilder[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            answer[i]= new StringBuilder("");
+            String  binary = String.format("%16s",Integer.toBinaryString(arr1[i] | arr2[i]));
+            binary = binary.substring(binary.length()-arr1.length);
+            binary = binary.replace("1", "#");
+            binary = binary.replace("0", " ");
+            answer[i].append(binary);
+        }
+        return answer;
+    }
+}
