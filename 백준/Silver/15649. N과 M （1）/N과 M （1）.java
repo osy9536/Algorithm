baccd9f7c89a5c04558e59ccdb9b95a7785626cb
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-04-28 15:14:51 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 22832 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] used;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        used = new int[N + 1];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (used[i] == 1) {
+                continue;
+            }
+            used[i] = 1;
+            arr[depth] = i;
+            dfs(depth + 1);
+            used[i] = 0;
+
+        }
+    }
+}
