251c00700b411635cbba8ba1254f80ad3303783b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-05-01 23:20:07 +0900

    [D3] Title: 세영이의 SEM력 연도, Time: 157 ms, Memory: 27,504 KB -BaekjoonHub

--- SWEA/D3/7532. 세영이의 SEM력 연도/세영이의 SEM력 연도.java
+++ SWEA/D3/7532. 세영이의 SEM력 연도/세영이의 SEM력 연도.java
@@ -1,15 +1,20 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            int a1 = sc.nextInt() - 1;
-            int a2 = sc.nextInt() - 1;
-            int a3 = sc.nextInt() - 1;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a1 = Integer.parseInt(st.nextToken()) - 1;
+            int a2 = Integer.parseInt(st.nextToken()) - 1;
+            int a3 = Integer.parseInt(st.nextToken()) - 1;
 
             int x = a1;
 
