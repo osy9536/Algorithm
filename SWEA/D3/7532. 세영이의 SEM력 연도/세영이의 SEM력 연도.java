eb2df5a5d64188901a317a54aff689e782d4d8fa
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-05-01 23:24:37 +0900

    [D3] Title: 세영이의 SEM력 연도, Time: 129 ms, Memory: 24,472 KB -BaekjoonHub

--- SWEA/D3/7532. 세영이의 SEM력 연도/세영이의 SEM력 연도.java
+++ SWEA/D3/7532. 세영이의 SEM력 연도/세영이의 SEM력 연도.java
@@ -1,12 +1,15 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -21,7 +24,10 @@ public class Solution {
             while (x % 24 != a2 || x % 29 != a3) {
                 x += 365;
             }
-            System.out.println("#" + (i + 1) + " " + (x + 1));
+            bw.write("#" + (i + 1) + " " + (x + 1)+"\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
