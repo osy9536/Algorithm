28f6e6ced8ebd6ae4179012339ec4553f53518b1
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-05-04 01:44:02 +0900

    [D2] Title: Base64 Decoder, Time: 105 ms, Memory: 19,404 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -1,21 +1,48 @@
-import java.util.Base64;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // Base64 Decoder
-// 가랏! RC카!
-// 간단한 소인수분해
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            String s = sc.nextLine();
-            byte[] bytes = s.getBytes();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-            Base64.Decoder decoder = Base64.getDecoder();
-            byte[] decodeBytes = decoder.decode(bytes);
-            System.out.println("#" + (i + 1) + " " + new String(decodeBytes));
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < s.length(); j++) {
+                int decode=0;
+                if (s.charAt(j) >= 'A' && s.charAt(j) <= 'Z') {
+                    decode = s.charAt(j) - 65;
+                } else if (s.charAt(j) >= 'a' && s.charAt(j) <= 'z') {
+                    decode = s.charAt(j) - 71;
+                } else if (s.charAt(j) >= '0' && s.charAt(j) <= '9') {
+                    decode = s.charAt(j) + 4;
+                } else if (s.charAt(j)=='+') {
+                    decode = s.charAt(j) + 19;
+                } else if (s.charAt(j)=='/') {
+                    decode = s.charAt(j) + 17;
+                }
+                String binary = Integer.toBinaryString(decode);
+                StringBuilder add = new StringBuilder();
+                if (binary.length() < 6) {
+                    for (int k = 0; k < 6-binary.length(); k++) {
+                        add.append("0");                        
+                    }
+                }
+                sb.append(add).append(binary);
+            }
+            int m = 0;
+            StringBuilder decode = new StringBuilder();
+            while (m < sb.length()) {
+                int sub = Integer.parseInt(sb.substring(m, Math.min(m + 8, sb.length())),2);
+                char c = (char) (sub);
+                decode.append(c);
+                m += 8;
+            }
+            System.out.println("#"+(i+1)+" "+decode);
         }
     }
 }
