4fa82568c20dc90c8425960486424d0fd05d5053
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-08-02 11:38:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2292 ms, Memory: 62760 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr= new int[n+1];
+		
+		for(int i = 1; i<=n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 1; i<=n; i++) {
+			arr[i]=arr[i-1]+arr[i];
+		}
+		for(int i = 0; i<m ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int left =Integer.parseInt(st.nextToken());
+			int right = Integer.parseInt(st.nextToken());
+			
+			System.out.println(arr[right]-arr[left-1]);
+		}
+	}
+}
