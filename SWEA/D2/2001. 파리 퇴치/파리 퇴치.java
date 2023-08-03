c33d9f2e7b253f82e0179a7431a656970f5a6953
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-08-03 14:40:11 +0900

    [D2] Title: 파리 퇴치, Time: 206 ms, Memory: 27,236 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 파리 퇴치
+// D2
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			int[][] arr = new int[n][n];
+
+			for (int j = 0; j < n; j++) {
+				arr[j] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			}
+			int max = 0;
+
+			for (int j = 0; j < n - m+1 ; j++) {
+				for (int k = 0; k < n - m+1 ; k++) {
+					int sum = 0;
+					for (int mj = 0; mj < m; mj++) {
+						for (int mk = 0; mk < m; mk++) {
+							sum += arr[j + mj][k + mk];
+						}
+					}
+					if (sum > max)
+						max = sum;
+				}
+			}
+			System.out.println("#"+i+" "+max);
+		}
+	}
+}
