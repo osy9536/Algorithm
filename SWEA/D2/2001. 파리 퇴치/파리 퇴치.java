c4c8cb56d1b7e1b52badf0aca3dd7039243547a6
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-08-03 14:42:26 +0900

    [D2] Title: 파리 퇴치, Time: 195 ms, Memory: 26,876 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,7 +1,9 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -10,6 +12,7 @@ import java.util.StringTokenizer;
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int t = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= t; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -34,7 +37,8 @@ public class Solution {
 						max = sum;
 				}
 			}
-			System.out.println("#"+i+" "+max);
+			bw.write("#"+i+" "+max+"\n");
 		}
+		bw.flush();
 	}
 }
