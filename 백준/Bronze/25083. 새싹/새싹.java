1fde22b92481852aeb2d934e143042483cbccdca
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-08-13 12:55:09 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |\n");
+        System.out.println(sb.toString());
+    }
+}
