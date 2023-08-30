6679201c408c7eee558899dd5446209e9b70c6d0
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-08-30 17:16:21 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -2,56 +2,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 파이프 옮기기1
+// gold 5
 public class Main {
-	static int[] dx = { 0, 1, 1 };
-	static int[] dy = { 1, 0, 1 };
+	static int[][][] dp;
 	static int[][] map;
-	static int N, answer;
-	static boolean[][] visited;
+	static int N;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
+		dp = new int[N][N][3];
 		map = new int[N][N];
-		visited = new boolean[N][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		visited[0][0] = true;
-		dfs(0, 1,0);
-		System.out.println(answer);
+		dp[0][1][0]=1;
+		dp();
+		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
 	}
 
-	static void dfs(int x, int y, int dir) {
-		if (x == N - 1 && y == N - 1) {
-			answer++;
-		}
-		
-		if (!visited[x][y]) {
-			visited[x][y] = true;
-			for (int i = 0; i < 3; i++) {
-				if(dir==0) {
-					if(i==1)continue;
-				}
-				if(dir==1) {
-					if(i==0)continue;
-				}
-				int nx = x + dx[i];
-				int ny = y + dy[i]; 
-				if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] != 1 && !visited[nx][ny]) {
-					if(i==2) {
-						if(map[nx][y]!=1&&map[x][ny]!=1) {
-							dfs(nx,ny,i);
-						}
-					}else dfs(nx, ny,i);
+	static void dp() {
+		for (int x = 0; x < N; x++) {
+			for (int y = 1; y < N; y++) {
+				if (map[x][y] != 1) {
+					dp[x][y][0] += dp[x][y - 1][0] + dp[x][y - 1][2];
+					if(x==0)continue;
+					dp[x][y][1] += dp[x - 1][y][1] + dp[x - 1][y][2];
+					if(map[x-1][y]==0&&map[x][y-1]==0) {
+						dp[x][y][2]+=dp[x-1][y-1][0]+dp[x-1][y-1][1]+dp[x-1][y-1][2];
+					}
 				}
 			}
-			visited[x][y] = false;
 		}
 	}
 }
