1c8a036b8a0984b6ef929e6232c1aecec6489c1f
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-10-09 19:35:31 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1204 ms, Memory: 304188 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,54 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] num;
+    static boolean[] visited;
+    static boolean[] finished;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tc; i++) {
+            int n = Integer.parseInt(br.readLine());
+            num = new int[n];
+            visited = new boolean[n];
+            finished = new boolean[n];
+            answer = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int a = Integer.parseInt(st.nextToken()) - 1;
+                num[j] = a;
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (!visited[j]) {
+                    dfs(j);
+                }
+            }
+            sb.append(n - answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int start) {
+        visited[start] = true;
+        int next = num[start];
+
+        if (!visited[next]) {
+            dfs(next);
+        } else {
+            if (!finished[next]) {
+                for (int i = next; i != start; i = num[i]) {
+                    answer++;
+                }
+                answer++; // 사이클의 길이를 더해줍니다.
+            }
+        }
+        finished[start] = true;
+    }
+}
