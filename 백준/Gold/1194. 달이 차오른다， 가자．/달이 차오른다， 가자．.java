7e04c29a7ae36ae9554c890710ebe67f455d85c3
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-10-09 16:32:14 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 108 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -9,15 +8,13 @@ import java.util.StringTokenizer;
 public class Main {
 
     static class Node {
-        int alphabet, x, y, depth;
-        boolean[] hasKey;
+        int x, y, alphabet, depth;
 
-        Node(int x, int y, int alphabet, int depth, boolean[] hasKey) {
+        Node(int x, int y, int alphabet, int depth) {
             this.x = x;
             this.y = y;
             this.alphabet = alphabet;
             this.depth = depth;
-            this.hasKey = hasKey;
         }
     }
 
@@ -35,7 +32,7 @@ public class Main {
         rowN = Integer.parseInt(st.nextToken());
         colN = Integer.parseInt(st.nextToken());
         map = new char[rowN][colN];
-        visited = new boolean[rowN][colN][92];
+        visited = new boolean[rowN][colN][1 << 7];
         int startX = 0;
         int startY = 0;
         for (int i = 0; i < rowN; i++) {
@@ -49,14 +46,13 @@ public class Main {
             }
         }
 
-
-        System.out.print(BFS(startX, startY, 0, 0));
+        System.out.print(BFS(startX, startY));
     }
 
-    private static int BFS(int startX, int startY, int alphabet, int depth) {
+    private static int BFS(int startX, int startY) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(startX, startY, alphabet, depth, new boolean[7]));
-        visited[startX][startY][alphabet] = true;
+        q.add(new Node(startX, startY, 0, 0));
+        visited[startX][startY][0] = true;
         while (!q.isEmpty()) {
             Node cur = q.poll();
             if (map[cur.x][cur.y] == '1') {
@@ -67,20 +63,21 @@ public class Main {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
                 int alpha = cur.alphabet;
-                boolean[] keys = cur.hasKey.clone();
+
                 if (nx >= 0 && ny >= 0 && nx < rowN && ny < colN && map[nx][ny] != '#') {
                     if (map[nx][ny] >= 'a' && map[nx][ny] <= 'f') {
-                        if(!keys[map[nx][ny]-'a']) alpha += (map[nx][ny] - 'a'+1)*(map[nx][ny] - 'a'+1);
-                        keys[map[nx][ny] - 'a'] = true;
+                        int key = 1 << (map[nx][ny] - 'a');
+                        alpha |= key;
                     }
                     if (map[nx][ny] >= 'A' && map[nx][ny] <= 'F') {
-                        if (!cur.hasKey[map[nx][ny] - 'A']) {
+                        int door = 1 << (map[nx][ny] - 'A');
+                        if ((alpha & door) == 0) {
                             continue;
                         }
                     }
                     if (visited[nx][ny][alpha]) continue;
                     visited[nx][ny][alpha] = true;
-                    q.add(new Node(nx, ny, alpha, cur.depth + 1,keys));
+                    q.add(new Node(nx, ny, alpha, cur.depth + 1));
                 }
             }
         }
