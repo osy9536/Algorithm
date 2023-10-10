bb78a7aa3f13d58a93f546730b4068d9260a841b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-10-10 23:19:35 +0900

    [Gold I] Title: 외판원 순회, Time: 180 ms, Memory: 19400 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 16000000;
+    static int[][] map;
+    static int[][] dp;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        dp = new int[n][1 << n]; // 비트마스킹
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(dfs(0, 1));
+    }
+
+    // 최단 거리 구하기
+    private static int dfs(int city, int visited) {
+        if (visited == (1 << n) - 1) {
+            if (map[city][0] == 0) {
+                return INF;
+            }
+            return map[city][0];
+        }
+
+        if (dp[city][visited] != 0) {
+            return dp[city][visited];
+        }
+
+        int minDistance = INF;
+        for (int i = 0; i < n; i++) {
+            if ((visited & (1 << i)) == 0 && map[city][i] != 0) {
+                int distance = dfs(i, visited | (1 << i)) + map[city][i];
+                minDistance = Math.min(minDistance, distance);
+            }
+        }
+
+        dp[city][visited] = minDistance;
+        return minDistance;
+    }
+}
