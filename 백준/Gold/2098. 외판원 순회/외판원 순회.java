d972dcf3fc1d853580d264d620599b4a27d6013f
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-10-11 16:06:30 +0900

    [Gold I] Title: 외판원 순회, Time: 124 ms, Memory: 16244 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,53 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int INF = 5000000;
-    static int[][] map;
-    static int[][] dp;
-    static int n;
+	static int[][] map, dp;
+	static int n, end, INF = 5000000;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        dp = new int[n][1 << n]; // 비트마스킹
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		map = new int[n][n];
+		dp = new int[n][1 << n];
+		end = (1 << n) - 1;
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		n--;
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		System.out.println(dfs(0, 1));
+	}
 
-        System.out.println(dfs(0, 1));
-    }
+	private static int dfs(int now, int visited) {
+		if (visited == end) {
+			if (map[now][0] == 0)
+				return 5000001;
+			return map[now][0];
+		}
 
-    // 최단 거리 구하기
-    private static int dfs(int city, int visited) {
-        if (visited == (1 << n) - 1) {
-            if (map[city][0] == 0) {
-                return 11000000;
-            }
-            return map[city][0];
-        }
+		if (dp[now][visited] != 0)
+			return dp[now][visited];
 
-        if (dp[city][visited] != 0) {
-            return dp[city][visited];
-        }
-
-        int minDistance = INF;
-        for (int i = 0; i < n; i++) {
-            if ((visited & (1 << i)) == 0 && map[city][i] != 0) {
-                int distance = dfs(i, visited | (1 << i)) + map[city][i];
-                minDistance = Math.min(minDistance, distance);
-            }
-        }
-
-        dp[city][visited] = minDistance;
-        return minDistance;
-    }
+		int min = INF;
+		for (int i = n; i >= 0; i--) {
+			if ((visited & (1 << i)) > 0)
+				continue;
+			if (map[now][i] == 0)
+				continue;
+			min = Math.min(min, map[now][i] + dfs(i, visited | (1 << i)));
+		}
+		
+		return dp[now][visited] = min;
+	}
 }
