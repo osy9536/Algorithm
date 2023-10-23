9c05db82caf667fd06ca85d50c53ce4ac66c050b
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-10-23 17:06:03 +0900

    [Gold II] Title: 문제집, Time: 480 ms, Memory: 44552 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// 뮨제집
+// gold 2
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        int[] inDegree = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            inDegree[b]++;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                pq.offer(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
+            sb.append(current).append(" ");
+
+            for (int next : graph[current]) {
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    pq.offer(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
