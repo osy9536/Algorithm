2f2aac9eb52fe74efe76aa01b467776c1eb2bdcc
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2023-11-15 22:52:58 +0900

    [Platinum III] Title: 전설, Time: 6152 ms, Memory: 875968 KB -BaekjoonHub

--- 백준/Platinum/19585. 전설/전설.java
+++ 백준/Platinum/19585. 전설/전설.java
@@ -1,94 +1,98 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Trie {
-    boolean isWordEnd;
-    Trie[] child;
-
-    public Trie() {
-        isWordEnd = false;
-        child = new Trie[26];
-    }
-
-    public Trie addAndGetChild(char c) {
-        if (child[c-'a'] == null)
-            child[c-'a'] = new Trie();
-        return child[c-'a'];
-    }
-
-    public Trie getChild(char c) {
-        return child[c-'a'];
-    }
-
-    public void setWordEnd() {
-        this.isWordEnd = true;
-    }
-}
-
+// 전설
+// platinum 3
 public class Main {
-    Trie cTrie = new Trie();    // 색상 트라이
-    HashSet<String> nHs = new HashSet<>();
-    int cMin, nMin, cMax, nMax;
 
-    private boolean searchTeamName(String s) {
-        int len = s.length();
-        // 우선 팀명에 대해 앞 부분 부터 보면서 색상과 매칭되는걸 colorMatched에 기록
-        Trie iter = cTrie;
-        for (int i = 0; i < len; i++) {
-            if (len-i < nMin)
-                break;
-            iter = iter.getChild(s.charAt(i));
-            if (iter == null) break;
-            if (iter.isWordEnd) {
-                if (nHs.contains(s.substring(i+1)))
-                    return true;
-            }
+    static class Node {
+        Node(){
+            childNode = new Node[26];
+            endOfWord = false;
         }
-        return false;
+        // 자식노드
+//        Map<Character, Node> childNode = new HashMap<>();
+        Node[] childNode;
+        boolean endOfWord;
     }
 
-    private void solution() throws Exception {
+    static class Trie {
+
+        Node rootNode = new Node();
+
+        void insert(String s) {
+            Node node = this.rootNode;
+//            for (int i = 0; i < s.length(); i++) {
+//                node = node.childNode.computeIfAbsent(s.charAt(i), key -> new Node());
+//            }
+            for (int i = 0; i < s.length(); i++) {
+//                char ch = s.charAt(i);
+//                if (!node.childNode.containsKey(ch)) {
+//                    node.childNode.put(ch, new Node());
+//                }
+//                node = node.childNode.get(ch);
+
+                char c = s.charAt(i);
+                int index = c - 'a';
+                if (node.childNode[index] == null)
+                    node.childNode[index] = new Node();
+                node = node.childNode[index];
+            }
+            node.endOfWord = true;
+        }
+
+        String search(String s) {
+            Node node = this.rootNode;
+            for (int i = 0; i < s.length(); i++) {
+                if (node.endOfWord) {
+                    if (names.contains(s.substring(i))) {
+                        return "Yes";
+                    }
+                }
+//                char c = s.charAt(i);
+                char c = s.charAt(i);
+                int index = c - 'a';
+//                if(node.childNode.get(c)==null) return false;
+                if(node.childNode[index]==null) break;
+                node = node.childNode[index];
+            }
+
+            return "No";
+        }
+    }
+
+    static Trie colors;
+    static Set<String> names;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int c = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        cMin = nMin = 1001;
-        cMax = nMax = 0;
+
+        colors = new Trie();
+        names = new HashSet<>();
+
         for (int i = 0; i < c; i++) {
-            String cur = br.readLine();
-            int len = cur.length();
-            if (len < cMin) cMin = len;
-            if (len > cMax) cMax = len;
-            // 이하 색상 트라이 삽입
-            Trie iter = cTrie;
-            for (int j = 0; j < len; j++) {
-                iter = iter.addAndGetChild(cur.charAt(j));
-            }
-            iter.setWordEnd();
+            String color = br.readLine();
+            colors.insert(color);
         }
         for (int i = 0; i < n; i++) {
-            String cur = br.readLine();
-            int len = cur.length();
-            if (len < nMin) nMin = len;
-            if (len > nMax) nMax = len;
-            nHs.add(cur);
+            String name = br.readLine();
+            names.add(name);
         }
+
         int q = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-        while (q-->0) {
-            String cur = br.readLine();
-            boolean chk = cur.length() > cMax + nMax ? false : searchTeamName(cur);
-            if (chk) {
-                sb.append('Y').append('e').append('s').append('\n');
-            } else {
-                sb.append('N').append('o').append('\n');
-            }
+        for (int i = 0; i < q; i++) {
+            String team = br.readLine();
+            sb.append(colors.search(team)).append("\n");
         }
-        System.out.print(sb);
-    }
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
+
+        System.out.println(sb);
     }
 }
