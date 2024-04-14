d029474b37774067377901ab2db2f406460b0ba7
Seyoung Oh <76714304+osy9536@users.noreply.github.com>
2024-04-14 13:32:08 +0900

    [level 2] Title: 전화번호 목록, Time: 446.01 ms, Memory: 176 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i<phone_book.length; i++){
+            map.putIfAbsent(phone_book[i],i);
+        }
+        
+        for(String s : phone_book){
+            for(int i = 0; i<s.length(); i++){
+                if(map.containsKey(s.substring(0,i))){
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    }
+}
